2b1c8f2996ac83e56b8c499405200d3d7890d11b
Daniel Walker <danwalker1990@outlook.com>
2022-04-24 20:34:41 +0100

    Update Help Get-Net2Timezones

--- PSNet2Api.psm1
+++ PSNet2Api.psm1
@@ -339,22 +339,26 @@ function Get-Net2ServerProperties {
 
 <#
 .SYNOPSIS
-Short description
+Returns a list of timezones
 
 .DESCRIPTION
-Long description
+Returns a list of timezones, Can be filtered to a single timezone with
+additional details
 
 .PARAMETER TimezoneId
-Parameter description
+Id of timezone to return
 
 .PARAMETER Detail
-Parameter description
+Optional switch to return additional details about the timezone
 
 .EXAMPLE
-An example
+Get-Net2Timezones
 
-.NOTES
-General notes
+.EXAMPLE
+Get-Net2Timezones -TiemzoneId 4
+
+.EXAMPLE
+Get-Net2Timezones -TimezoneId 4 -Detail
 #>
 function Get-Net2Timezones {
     [CmdletBinding(DefaultParameterSetName = "All")]
