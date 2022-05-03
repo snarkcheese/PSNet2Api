dcfc80b7196f742142fdda0b5ecdaca563d2d1d2
Daniel Walker <danwalker1990@outlook.com>
2022-05-03 12:03:50 +0100

    NEw functions

--- PSNet2Api.psm1
+++ PSNet2Api.psm1
@@ -1007,18 +1007,104 @@ function New-AccessLevel {
 
         [int]$AccessLevelId
     )
+    $new = $false
     $t = @{}
     if ($AccessLevelName) {
         $t.add("name", $AccessLevelName)
+        $new = $true
     }
     if ($AccessLevelId) {
         $t.add("id", $AccessLevelId)
+        $new = $true
     }
     if ($AccessLevelDetail) {
         $t.add("detailRows", $AccessLevelDetail)
+        $new = $true
     }
-    if ($t -ne @{}) {
+    if ($new) {
         $body = ConvertTo-Json -InputObject $t -Depth 5 -Compress
-        Invoke-Net2ApiCall -Endpoint "/api/accesslevels" -Method Post -Body $body
+        Invoke-Net2ApiCall -Endpoint "/api/v1/accesslevels" -Method "Post" -Body $body
+    }
+}
+
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Long description
+
+.PARAMETER AccessLevelId
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+function Remove-AccessLevel {
+    param (
+        [parameter(Mandatory, Position = 0)]
+        [int]$AccessLevelId
+    )
+    $endpoint = "/api/v1/accesslevels/{0}" -f $AccessLevelId
+    Invoke-Net2ApiCall -Endpoint $endpoint -Method "Delete"
+}
+
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Long description
+
+.PARAMETER AccessLevelId
+Parameter description
+
+.PARAMETER NewAccessLevelId
+Parameter description
+
+.PARAMETER AccessLevelDetail
+Parameter description
+
+.PARAMETER NewAccessLevelName
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+function Set-AccessLevel {
+    param(
+        [parameter(Mandatory, Position = 0)]
+        [int]$AccessLevelId,
+
+        [int]$NewAccessLevelId,
+
+        [object[]]$AccessLevelDetail,
+
+        [string]$NewAccessLevelName
+    )
+    $change = $false
+    $t = @{}
+    if ($NewAccessLevelId) {
+        $t.Add("id", $NewAccessLevelId)
+        $change = $true
+    }
+    if ($AccessLevelDetail) {
+        $t.Add("detailRows", $AccessLevelDetail)
+        $change = $true
+    }
+    if ($NewAccessLevelName) {
+        $t.Add("name", $NewAccessLevelName)
+        $change = $true
+    }
+    if ($change) {
+        $endpoint = "/api/v1/accesslevels/{0}" -f $AccessLevelId
+        $body = ConvertTo-Json -InputObject $t -Depth 5
+        Invoke-Net2ApiCall -Endpoint $endpoint -Body $body -Method "Put"
     }
 }
