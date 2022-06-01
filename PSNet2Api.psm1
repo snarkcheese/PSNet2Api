de643efd61fb85a0ddec0fcb58d1df09d15d099e
Daniel Walker <danwalker1990@outlook.com>
2022-06-01 10:06:15 +0100

    Remove-Net2Department

--- PSNet2Api.psm1
+++ PSNet2Api.psm1
@@ -1447,10 +1447,29 @@ function Set-Net2Department {
     Invoke-Net2ApiCall -Endpoint $endpoint -Method Put -Body $body
 }
 
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Long description
+
+.PARAMETER DepartmentId
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
 function Remove-Net2Department {
     param(
-
+        [parameter(Mandatory, Position = 0)]
+        [int]$DepartmentId
     )
+    $endpoint = "/api/v1/departments/{0}" -f $DepartmentId
+    Invoke-Net2ApiCall -Endpoint $endpoint -Method Delete
 }
 
 function Set-Net2UserDepartment {
